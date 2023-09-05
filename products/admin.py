5e7c135177023d0a9400024a69368dc50cac34b6
xamidjonovk <77206354+xamidjonovk@users.noreply.github.com>
2023-09-05 21:37:04 +0500

    add review to admin site

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Order, Product
+from .models import Order, Product, Review
 
 admin.site.register(Order)
 admin.site.register(Product)
+admin.site.register(Review)
